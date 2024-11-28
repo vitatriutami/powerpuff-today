2397da3c53ddd3220891c21490460849727bbefb
Vita <vitatriutami@gmail.com>
2024-11-29 03:37:27 +0700

    add: fix button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,102 +49,106 @@ const Home: React.FC = () => {
   return (
     <div className="py-6 px-16 h-[80vh]">
       <main className="flex flex-col items-center justify-center md:justify-between gap-8">
-      <nav className="flex gap-2 justify-between px-8 py-4 border-[3px] border-black rounded-lg w-full">
-        <div>Logo</div>
-        <div className="flex gap-1">
-          <img src="/blossom.svg" width={50} alt="Blossom" />
-        </div>
-        <div>About</div>
-      </nav>
-      <section className="grid grid-cols-4 gap-2 sm:gap-4 md:gap-7 md:grid-rows-[1fr_auto]">
-        {[
-          {
-            bg: "bg-gradient-to-r from-slate-100 via-purple-300 to-slate-100",
-            src: "/pngegg (1).png",
-            isRowSpan: true,
-            name: "Powerpuff Girls",
-            desc: "Superhero in Townsville",
-          },
-          {
-            bg: "bg-gradient-to-b from-sky-300 via-cyan-100 to-sky-300",
-            src: "/pngegg (4).png",
-            name: "Bubbles",
-            desc: "The joy and the laughter",
-          },
-          {
-            bg: "bg-gradient-to-b from-pink-300 via-purple-100 to-pink-300",
-            src: "/pngegg5.png",
-            name: "Blossom",
-            desc: "The commander and the leader",
-          },
-          {
-            bg: "bg-gradient-to-b from-emerald-300 via-green-100 to-emerald-300",
-            src: "/pngegg (3).png",
-            name: "Buttercup",
-            desc: "The toughest fighter",
-          },
-        ].map((item, index) => (
-          // Group per icon box
-          <div
-            key={index}
-            className={`${
-              item.bg
-            } group relative flex items-center justify-center overflow-hidden rounded-lg border-2 ${
-              item.isRowSpan ? "md:row-span-2 h-full" : "h-56 md:h-64"
-            }`}
-          >
+        <nav className="flex gap-2 justify-between px-8 py-4 border-[3px] border-black rounded-lg w-full">
+          <div>Logo</div>
+          <div className="flex gap-1">
+            <img src="/blossom.svg" width={50} alt="Blossom" />
+          </div>
+          <div>About</div>
+        </nav>
+        <section className="grid grid-cols-4 gap-2 sm:gap-4 md:gap-7 md:grid-rows-[1fr_auto]">
+          {[
+            {
+              bg: "bg-gradient-to-r from-slate-100 via-purple-300 to-slate-100",
+              src: "/pngegg (1).png",
+              isRowSpan: true,
+              name: "Powerpuff Girls",
+              desc: "Superhero in Townsville",
+            },
+            {
+              bg: "bg-gradient-to-b from-sky-300 via-cyan-100 to-sky-300",
+              src: "/pngegg (4).png",
+              name: "Bubbles",
+              desc: "The joy and the laughter",
+            },
+            {
+              bg: "bg-gradient-to-b from-pink-300 via-purple-100 to-pink-300",
+              src: "/pngegg5.png",
+              name: "Blossom",
+              desc: "The commander and the leader",
+            },
+            {
+              bg: "bg-gradient-to-b from-emerald-300 via-green-100 to-emerald-300",
+              src: "/pngegg (3).png",
+              name: "Buttercup",
+              desc: "The toughest fighter",
+            },
+          ].map((item, index) => (
+            // Group per icon box
             <div
-              className={`absolute bottom-[-32px] left-0 hidden w-full border-t-2 px-4 py-2 text-center ${
-                item.src === "/pngegg (1).png"
-                  ? "group-hover:text-center"
-                  : "group-hover:text-start"
-              } text-black bg-slate-300 transition-transform duration-300 ease-out group-hover:translate-y-[-32px] md:block`}
+              key={index}
+              className={`${
+                item.bg
+              } group relative flex items-center justify-center overflow-hidden rounded-lg border-2 ${
+                item.isRowSpan ? "md:row-span-2 h-full" : "h-56 md:h-64"
+              }`}
             >
-              <span className="text-4xl uppercase font-ppg text-black tracking-wider">
-                {item.name}
-              </span>
-              <p className="line-clamp-2 h-8 leading-4 mt-2 text-xl">
-                {item.desc}
-              </p>
+              <div
+                className={`absolute bottom-[-32px] left-0 hidden w-full border-t-2 px-4 py-2 text-center ${
+                  item.src === "/pngegg (1).png"
+                    ? "group-hover:text-center"
+                    : "group-hover:text-start"
+                } text-black bg-slate-300 transition-transform duration-300 ease-out group-hover:translate-y-[-32px] md:block`}
+              >
+                <span className="text-4xl uppercase font-ppg text-black tracking-wider">
+                  {item.name}
+                </span>
+                <p className="line-clamp-2 h-8 leading-4 mt-2 text-xl">
+                  {item.desc}
+                </p>
+              </div>
+              <Image
+                src={item.src}
+                width={item.src === "/pngegg (1).png" ? 300 : 130}
+                height={item.src === "/pngegg (1).png" ? 300 : 130}
+                alt="Powerpuff Girl"
+                className={`transition-transform ease-out ${
+                  item.src === "/pngegg (1).png" && item.isRowSpan
+                    ? ""
+                    : "md:group-hover:translate-x-[55px]"
+                } group-hover:translate-y-[-6px] duration-300 ease-out group-hover:scale-75 md:group-hover:scale-75`}
+              />
             </div>
-            <Image
-              src={item.src}
-              width={item.src === "/pngegg (1).png" ? 300 : 130}
-              height={item.src === "/pngegg (1).png" ? 300 : 130}
-              alt="Powerpuff Girl"
-              className={`transition-transform ease-out ${
-                item.src === "/pngegg (1).png" && item.isRowSpan
-                  ? ""
-                  : "md:group-hover:translate-x-[55px]"
-              } group-hover:translate-y-[-6px] duration-300 ease-out group-hover:scale-75 md:group-hover:scale-75`}
-            />
-          </div>
-        ))}
-        {/* Second Row Content */}
-        <div className="col-span-4 md:col-span-3 md:col-start-2">
-          <div className="mt-8 flex flex-col gap-3 sm:mt-0 sm:gap-3 font-ticketing">
-            <div>
-              <h1 className="text-3xl md:text-7xl uppercase leading-9 tracking-tight">
-                Which Powerpuff Girls are You Today?
-              </h1>
-              <p className="mb-2 text-3xl md:text-3xl">
-                Ready to uncover your inner hero? <span className="font-bold text-purple-400">Blastpuff Match</span> is a fun,
-                interactive journey that matches you with a Powerpuff-inspired
-                character based on your unique daily experiences. Every day
-                brings a new adventure, and who you are today might surprise
-                you! Dive in, explore your story, and let the blast begin!
-              </p>
+          ))}
+          {/* Second Row Content */}
+          <div className="col-span-4 md:col-span-3 md:col-start-2">
+            <div className="mt-8 flex flex-col gap-3 sm:mt-0 sm:gap-3 font-ticketing">
+              <div>
+                <h1 className="text-3xl md:text-7xl uppercase leading-9 tracking-tight">
+                  Which Powerpuff Girls are You Today?
+                </h1>
+                <p className="mb-2 text-3xl md:text-3xl">
+                  Ready to uncover your inner hero?{" "}
+                  <span className="font-bold text-purple-400">
+                    Blastpuff Match
+                  </span>{" "}
+                  is a fun, interactive journey that matches you with a
+                  Powerpuff-inspired character based on your unique daily
+                  experiences. Every day brings a new adventure, and who you are
+                  today might surprise you! Dive in, explore your story, and let
+                  the blast begin!
+                </p>
+              </div>
+              <Link
+                href="#"
+                className="w-[300px] text-4xl uppercase text-center group bg-black font-semibold py-4 px-6 rounded-lg shadow-md overflow-hidden outline-none focus:ring-4 transform active:scale-75 transition-transform"
+              >
+                <TextShimmerColor>Play now</TextShimmerColor>
+              </Link>
             </div>
-            <Link
-              href="#"
-              className="w-[300px] text-4xl uppercase text-center group bg-black font-semibold py-4 px-6 rounded-lg shadow-md overflow-hidden"
-            >
-              <TextShimmerColor>Play now</TextShimmerColor>
-            </Link>
           </div>
-        </div>
-      </section>
- </main>
+        </section>
+      </main>
       {/* <section className="bg-rose-300 p-8 rounded-lg shadow-xl">
         <h1 className="text-3xl font-bold mb-6 text-center">
           Which Powerpuff Are You Today?
--- src/components/motion-ui/TextShimmerColor.tsx
+++ src/components/motion-ui/TextShimmerColor.tsx
@@ -1,14 +1,14 @@
-import { TextShimmer } from '@/components/core/text-shimmer';
+import TextShimmer from '@/components/core/text-shimmer';
 
 interface Props {
-  children: ReactNode;
+  children: string; // Restrict children to a string
 }
 
 export default function TextShimmerColor({ children }: Props) {
   return (
     <TextShimmer
       duration={1.2}
-      className='[--base-color:theme(colors.blue.600)] [--base-gradient-color:theme(colors.blue.200)] dark:[--base-color:theme(colors.blue.700)] dark:[--base-gradient-color:theme(colors.blue.400)]'
+      className='[--base-color:theme(colors.purple.300)] [--base-gradient-color:theme(colors.purple.100)] dark:[--base-color:theme(colors.blue.700)] dark:[--base-gradient-color:theme(colors.blue.400)]'
     >
       {children}
     </TextShimmer>
