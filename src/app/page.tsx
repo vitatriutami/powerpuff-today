c1a3f23145effeb9738ef5d4be5697670c0dc41a
Vita <vitatriutami@gmail.com>
2024-11-29 07:21:33 +0700

    fix: fix eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+// "use client"
 // pages/index.tsx
 // import { useState } from "react";
 // import SelectionForm from "@/components/SelectionForm";
@@ -6,45 +7,46 @@ import Link from "next/link";
 import Image from "next/image";
 import { TextLoop } from "@/components/motion-ui/TextLoop";
 
-type Mood = "happy" | "sad" | "angry" | "tired";
-type Productivity = "full" | "half" | "low";
-type Sociality = "hangout" | "helping" | "alone";
+// type Mood = "happy" | "sad" | "angry" | "tired";
+// type Productivity = "full" | "half" | "low";
+// type Sociality = "hangout" | "helping" | "alone";
 
-const getCharacter = (
-  mood: Mood,
-  productivity: Productivity,
-  sociality: Sociality
-) => {
-  if (mood === "happy" && productivity === "full" && sociality === "hangout") {
-    return "Bubbles: You’re cheerful and always ready for fun!";
-  } else if (
-    mood === "angry" &&
-    productivity === "half" &&
-    sociality === "alone"
-  ) {
-    return "Buttercup: Strong, independent, and prefers solitude.";
-  } else if (
-    mood === "sad" &&
-    productivity === "low" &&
-    sociality === "helping"
-  ) {
-    return "Blossom: Thoughtful, caring, and ready to help.";
-  } else {
-    return "You’re a unique mix of Powerpuff traits!";
-  }
-};
+// const getCharacter = (
+//   mood: Mood,
+//   productivity: Productivity,
+//   sociality: Sociality
+// ) => {
+//   if (mood === "happy" && productivity === "full" && sociality === "hangout") {
+//     return "Bubbles: You’re cheerful and always ready for fun!";
+//   } else if (
+//     mood === "angry" &&
+//     productivity === "half" &&
+//     sociality === "alone"
+//   ) {
+//     return "Buttercup: Strong, independent, and prefers solitude.";
+//   } else if (
+//     mood === "sad" &&
+//     productivity === "low" &&
+//     sociality === "helping"
+//   ) {
+//     return "Blossom: Thoughtful, caring, and ready to help.";
+//   } else {
+//     return "You’re a unique mix of Powerpuff traits!";
+//   }
+// };
 
 const Home: React.FC = () => {
-  // const [character, setCharacter] = useState<string | null>(null);
+//   const [_character, setCharacter] = useState<string | null>(null);
 
-  const handleFormSubmit = (
-    mood: Mood,
-    productivity: Productivity,
-    sociality: Sociality
-  ) => {
-    const result = getCharacter(mood, productivity, sociality);
-    setCharacter(result);
-  };
+//  const handleFormSubmit = (
+//   _mood: Mood,
+//   _productivity: Productivity,
+//   _sociality: Sociality
+// ) => {
+//   const result = getCharacter(_mood, _productivity, _sociality);
+//   setCharacter(result);
+// };
+
 
   return (
     <div className="py-6 px-16 h-[80vh]">
